a592f82e626a21c070e1f96db766f523b31a272c
Caleb Blake <Caleb Blake@192.168.2.51>
2015-07-09 00:37:50 -0400

    Made a stage for the world

--- core/src/com/mygdx/game/SideScroller.java
+++ core/src/com/mygdx/game/SideScroller.java
@@ -1,27 +1,11 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class SideScroller extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class SideScroller extends Game {
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 1, .5f);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		setScreen(new GameScreen());
 	}
 }
